86d593e29e7fb1de6ff71b37df6a6537bf9344ef
Tyler <tyler.neustaedter@umbrage.com>
2023-06-13 13:08:28 -0500

    test build

--- src/components/DestinyLogin.tsx
+++ src/components/DestinyLogin.tsx
@@ -8,7 +8,7 @@ const DestinyLogin = () => {
     const clientId = process.env.REACT_APP_CLIENT_ID;
     const clientSecret = process.env.REACT_APP_CLIENT_SECRET;
     const redirectUri = process.env.REACT_APP_REDIRECT_URI_LOCAL || 'fart';
-
+    console.log('hello')
     useEffect(() => {
         const code = new URLSearchParams(window.location.search).get('code');
       
